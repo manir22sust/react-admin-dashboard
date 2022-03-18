fa6188d9c65ceabd0858ead8a2eb1c9aabed9b04
Md. Manir Uddin <manir22sust@gmail.com>
2022-03-18 12:02:27 +0100

    commit

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import DataTable from "../../components/datatable/DataTable";
+import { Navbar } from "../../components/navbar/Navbar";
+import { Sidebar } from "../../components/sidebar/Sidebar";
+import "./list.scss";
+
+export const List = () => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <DataTable />
+      </div>
+    </div>
+  );
+};
